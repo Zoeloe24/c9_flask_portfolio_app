273825a5e0cf7166ee52ee117115a70286f04e5b
Zoeloe24 <74582947+Zoeloe24@users.noreply.github.com>
2021-05-15 21:08:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + likes to play around with Python
+	new_name = str (name) + "likes to play around with Python"
 	return render_template('index.html', name=new_name)
 
 
