aa1ce098037615e5af16fe6e6a84a4ed7344682f
Zoeloe24 <74582947+Zoeloe24@users.noreply.github.com>
2021-05-15 20:51:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = name + likes to play around with Python
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
